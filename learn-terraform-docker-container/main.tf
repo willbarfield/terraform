8ecc000fd66aea9a03bfdf556e12d1e963feb43f
William Barfield <willbarfield@yahoo.com>
2023-12-25 19:26:20 -0600

    updated the port number

--- learn-terraform-docker-container/main.tf
+++ learn-terraform-docker-container/main.tf
@@ -10,16 +10,15 @@ terraform {
 provider "docker" {}
 
 resource "docker_image" "nginx" {
-  name         = "nginx"
+  name         = "nginx:latest"
   keep_locally = false
 }
 
 resource "docker_container" "nginx" {
   image = docker_image.nginx.image_id
   name  = "tutorial"
-
   ports {
     internal = 80
-    external = 8000
+    external = 8080
   }
 }
